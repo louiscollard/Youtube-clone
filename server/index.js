86886527865431405c3c88474a4506a7bb610f76
Louis Collard <louiscollard2@gmail.com>
2022-09-11 17:02:23 +0200

    Add like and dislike

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
-// import commentRoutes from "./routes/comments.js";
+import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
-// app.use("/api/comments", commentRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.use((err, req, res, next) => {
 	const status = err.status || 500;
--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const CommentShema = new mongoose.Schema(
+const CommentSchema = new mongoose.Schema(
 	{
 		userId: {
 			type: String,
