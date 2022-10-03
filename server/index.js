0181441b8d774f5231cde38d8dd04712e37067f9
Louis Collard <louiscollard2@gmail.com>
2022-10-03 16:06:55 +0200

    Add Login

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -21,6 +22,7 @@ const connect = () => {
 		});
 };
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
